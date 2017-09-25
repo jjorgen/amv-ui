a090cfb150dee0d4f9b937d51c663e4f14ed4ceb
jorgej2 <jjorgensen2311@gmail.com>
2017-09-25 07:03:44 -0400

    Sending complete JSON object to server with PUT request. Changed from mining in Tomcat and JHot Draw to only 'JHot Draw version 4.12b'.

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -24,102 +24,141 @@ var myApp = angular
             var url = putUrl();
             console.debug("url: " + url);
 
-            var data = {
-                "clusteringAlgorithm": "clusteringAlgorithm",
-                "vectorSpaceModel": "vectorSpaceModel",
-                "orderedMethodCall": "yes",
-                "codeClone":  "yes",
-                "uniqueClassFanIn":  "yes",
-                "callsInClones":  "yes",
-                "callsAtBeginningAndEndOfMethod": "no",
-                "eventAsParameter": "no",
-                "singleton": "no",
-                "observer": "no",
-                "chainOfResponsibility": "no",
-                "crossCuttingConcernAsInterface": "no"
-            };
+            var data = getAspectMiningInputParameters();
+            console.debug("data: " + data);
 
             $http.put(url, data)
                 .then(
                     function(response){
-                        $scope.product = response.data;
+                        $scope.aspectMiningResult = response.data;
                     },
                     function(response){
-                        $scope.product = response.data;
+                        $scope.aspectMiningResult = response.data;
                     }
                 );
-
-            // $http.put(url)
-            //     .then(function(response) {
-            //         $scope.product = response.data;
-            //     });
-
-            // $http.get(url)
-            //     .then(function(response) {
-            //         $scope.product = response.data;
-            //     });
-
         };
-
-        // $scope.mineForAspects = function() {
-        //     var url = getUrl();
-        //     console.debug("url: " + url);
-        //
-        //     $http.get(url)
-        //         .then(function(response) {
-        //             $scope.product = response.data;
-        //         });
-        //
-        //     $http.get(url)
-        //         .then(function(response) {
-        //             $scope.product = response.data;
-        //         });
-        // };
 
         function putUrl() {
             var url = 'http://localhost:8080/amvRestServices/service/loan/updateAspectMine';
             return url;
         }
 
-        function getUrl() {
-            var url = 'http://localhost:8080/amvRestServices/service/loan/-1?repayAmount=500.00&repayDate=2017-04-12';
-            return url;
+        function getAspectMiningInputParameters() {
+            var clusteringAlgorithm = getClusteringAlgorithm();
+            var vectorSpaceModel = getVectorSpaceModel()
+
+            var orderedMethodCall = getOrderedMethodCall();
+            var codeClone = getCodeClone();
+            var uniqueClassFanIn = getUniqueClassFanIn();
+            var callsInClones = getCallsInClones();
+            var callsAtBeginningAndEndOfMethod = getCallsAtBeginningAndEndOfMethod();
+            var eventAsParameter = getEventAsParameter();
+            var singleton = getSingleton();
+            var observer = getObserver();
+            var chainOfResponsibility = getChainOfResponsibility();
+            var crossCuttingConcernAsInterface = getCrossCuttingConcernAsInterface();
+
+            var data = {
+                "clusteringAlgorithm": clusteringAlgorithm,
+                "vectorSpaceModel": vectorSpaceModel,
+                "orderedMethodCall": orderedMethodCall,
+                "codeClone":  codeClone,
+                "uniqueClassFanIn":  uniqueClassFanIn,
+                "callsInClones":  callsInClones,
+                "callsAtBeginningAndEndOfMethod": callsAtBeginningAndEndOfMethod,
+                "eventAsParameter": eventAsParameter,
+                "singleton": singleton,
+                "observer": observer,
+                "chainOfResponsibility": chainOfResponsibility,
+                "crossCuttingConcernAsInterface": crossCuttingConcernAsInterface
+            };
+
+            return data;
         }
-
-        // function getUrl() {
-        //     var url = 'http://localhost:8080/amvRestServices/service/loan/-1?repayAmount=500.00&repayDate=2017-04-12';
-        //     return url;
-        // }
-
-        // function getUrl() {
-        //     var url = 'http://localhost:9191/amv/mine?clusteringAlgorithm=' + getClusteringAlgorithm();
-        //     url += '&vectorSpaceModel=' + getVectorSpaceModel();
-        //     url += '&crosscuttingConcerns=' + getCrossCuttingConcerns();
-        //     return url;
-        // }
 
         function getClusteringAlgorithm() {
             return aspectMine.clusteringAlgorithm;
         }
-
         function getVectorSpaceModel() {
             return aspectMine.vectorSpaceModel;
         }
-
-        function getCrossCuttingConcerns() {
-            var crossCuttingConcernList = '';
-            crossCuttingConcernList += aspectMine.orderedMethodCall ? '1' : '0';
-            crossCuttingConcernList += aspectMine.codeClone ? '1' : '0';
-            crossCuttingConcernList += aspectMine.uniqueClassFanIn ? '1' : '0';
-            crossCuttingConcernList += aspectMine.callsInClones ? '1' : '0';
-            crossCuttingConcernList += aspectMine.callsAtBeginningAndEndOfMethod ? '1' : '0';
-            crossCuttingConcernList += aspectMine.eventAsParameter ? '1' : '0';
-            crossCuttingConcernList += aspectMine.singleton ? '1' : '0';
-            crossCuttingConcernList += aspectMine.observer ? '1' : '0';
-            crossCuttingConcernList += aspectMine.chainOfResponsibility ? '1' : '0';
-            crossCuttingConcernList += aspectMine.crossCuttingConcernAsInterface ? '1' : '0';
-            return crossCuttingConcernList;
+        function getOrderedMethodCall() {
+            return aspectMine.orderedMethodCall;
         }
-
-        console.debug("After invoking GET method to obtain Product object");
+        function getCodeClone() {
+            return aspectMine.codeClone;
+        }
+        function getUniqueClassFanIn() {
+            return aspectMine.uniqueClassFanIn;
+        }
+        function getCallsInClones() {
+            return aspectMine.callsInClones;
+        }
+        function getCallsAtBeginningAndEndOfMethod() {
+            return aspectMine.callsAtBeginningAndEndOfMethod;
+        }
+        function getEventAsParameter() {
+            return aspectMine.eventAsParameter;
+        }
+        function getSingleton() {
+            return aspectMine.singleton;
+        }
+        function getObserver() {
+            return aspectMine.observer;
+        }
+        function getChainOfResponsibility() {
+            return aspectMine.chainOfResponsibility;
+        }
+        function getCrossCuttingConcernAsInterface() {
+            return aspectMine.crossCuttingConcernAsInterface;
+        }
+        console.debug("After invoking PUT method to get Aspect Mining Results");
     });
+
+// $scope.mineForAspects = function() {
+//     var url = getUrl();
+//     console.debug("url: " + url);
+//
+//     $http.get(url)
+//         .then(function(response) {
+//             $scope.product = response.data;
+//         });
+//
+//     $http.get(url)
+//         .then(function(response) {
+//             $scope.product = response.data;
+//         });
+// };
+
+// function getUrl() {
+//     var url = 'http://localhost:8080/amvRestServices/service/loan/-1?repayAmount=500.00&repayDate=2017-04-12';
+//     return url;
+// }
+
+// function getUrl() {
+//     var url = 'http://localhost:8080/amvRestServices/service/loan/-1?repayAmount=500.00&repayDate=2017-04-12';
+//     return url;
+// }
+
+// function getUrl() {
+//     var url = 'http://localhost:9191/amv/mine?clusteringAlgorithm=' + getClusteringAlgorithm();
+//     url += '&vectorSpaceModel=' + getVectorSpaceModel();
+//     url += '&crosscuttingConcerns=' + getCrossCuttingConcerns();
+//     return url;
+// }
+
+
+// function getCrossCuttingConcerns() {
+//     var crossCuttingConcernList = '';
+//     crossCuttingConcernList += aspectMine.orderedMethodCall ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.codeClone ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.uniqueClassFanIn ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.callsInClones ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.callsAtBeginningAndEndOfMethod ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.eventAsParameter ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.singleton ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.observer ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.chainOfResponsibility ? '1' : '0';
+//     crossCuttingConcernList += aspectMine.crossCuttingConcernAsInterface ? '1' : '0';
+//     return crossCuttingConcernList;
+// }
